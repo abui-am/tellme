10f6c0117dd9a885972fa5075e48df39b74f27a0
Abui <adjiem31@gmail.com>
2021-12-23 21:57:59 +0700

    add : config

--- firebase/nodeApp.js
+++ firebase/nodeApp.js
@@ -1,4 +1,4 @@
-import * as admin from 'firebase-admin'
+import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -7,8 +7,8 @@ if (!admin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  })
+    // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  });
 }
 
-export default admin
+export default admin;
